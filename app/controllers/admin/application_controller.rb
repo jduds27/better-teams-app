10f92734404b20a776859d81a6cd2591a22bb1ea
JJ Keeney <jjkeeney129285@gmail.com>
2017-11-10 15:37:43 -0700

    admin dashboard authentication

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -5,11 +5,21 @@
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
 module Admin
-  class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
 
+  def self.admin_types
+    ["admin"]
+  end
+
+  class ApplicationController < Administrate::ApplicationController
+    before_action :authenticate_user!
+    before_action :authenticate_admin
+    
     def authenticate_admin
-      # TODO Add authentication logic here.
+      unless Admin.admin_types.include?(current_user.roles)
+          flash[:notice] = "Dude you cant go here!"
+          redirect_to(root_path)
+        end
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
@@ -18,4 +28,4 @@ module Admin
     #   params[:per_page] || 20
     # end
   end
-end
+
