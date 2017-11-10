e080055eea0075bf6666470066ef6f36bb71308f
Jason Dudley <jason.dudley27@gmail.com>
2017-11-10 15:58:00 -0700

    resolved

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,12 +13,9 @@ class PagesController < ApplicationController
   end
 
   def show
-<<<<<<< HEAD
-=======
     @student = Student.find(params[:id])
   end
 
   def list
->>>>>>> b9f0c40151e8d9b796554152b7bb1285a002520a
   end
 end
